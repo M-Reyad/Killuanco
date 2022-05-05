88474039ec250213bb0c0c34079801e5c40dd37a
Mohamed Reyad <mohamed.reyad333@gmail.com>
2022-05-05 16:42:47 +0200

    Initial Commit

---
+++ Killuanco/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Killuanco
+//
+//  Created by Muhammad Reyadh on 5/5/22.
+//  Copyright © 2022 PA&A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
